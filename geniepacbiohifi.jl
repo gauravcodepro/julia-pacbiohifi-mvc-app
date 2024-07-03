4eeb1ec1d85b2c449a80a4706b7122a18efc2e8e
Gaurav <gaurav.sablok@uni-potsdam.de>
2024-07-03 22:55:15 +0200

    Update geniepacbiohifi.jl
    Signed-off-by: Gaurav <gaurav.sablok@uni-potsdam.de>

--- geniepacbiohifi.jl
+++ geniepacbiohifi.jl
@@ -52,8 +52,58 @@ function length(pacbiohififastq)
     end
 end 
 
-function displaypacbio(pacbiohififastq)
-   SearchLight.Migrations.init()
-    # make a backhand SQL database and store the reads for the dispatch and faster I/0 
-    conn = DBInterface.connect(MySQL.Connection, host, user, passwd) 
-    # add a struct here for the MySQL as prompt is not needed. 
+function pacbioKmerspace(pacbiohifireads, kmerspace)
+    header = Any[]
+    sequence = Any[]
+    for i in 1:length(readfile)
+        if startswith(readfile[i], "@")
+            push!(header,replace(split(readfile[i], r" ")[1], "@" => ""))
+            push!(sequence,readfile[i+1])
+        end
+    end
+    sequenctag = Any[]
+    for i in 1:length(sequence)
+        for j in 1:length(sequence[i])-kmerspace
+            push!(sequencetag, sequence[i][j:j+kmerspace])
+        end
+    end
+    uniquetag = Set(sequencetag)
+    uniquetagarray = Any[]
+    for i in uniquetag
+        push!(uniquetagarray, i)
+    end
+    counttag = Dict{String, Int64}()
+    for i in 1:length(uniquetagarray)
+        counttag[uniquetagarray[i]] = count(==unqiuetagarray[i], sequencetag)
+    end
+end
+
+function pacbioKmerspace(pacbiohifireads, kmerspace, threshold)
+    header = Any[]
+    sequence = Any[]
+    for i in 1:length(readfile)
+        if startswith(readfile[i], "@")
+            push!(header,replace(split(readfile[i], r" ")[1], "@" => ""))
+            push!(sequence,readfile[i+1])
+        end
+    end
+    sequenctag = Any[]
+    for i in 1:length(sequence)
+        for j in 1:length(sequence[i])-kmerspace
+            push!(sequencetag, sequence[i][j:j+kmerspace])
+        end
+    end
+    uniquetag = Set(sequencetag)
+    uniquetagarray = Any[]
+    for i in uniquetag
+        push!(uniquetagarray, i)
+    end
+    thresholdplaceholder = parse(Int64, threshold)
+    counttag = Dict{String, Int64}()
+    for i in 1:length(uniquetagarray)
+        if count(==uniquetagarray[i], sequencetage) > thresholdplaceholder
+        counttag[uniquetagarray[i]] = count(==unqiuetagarray[i], sequencetag)
+    end
+end 
+end
+
